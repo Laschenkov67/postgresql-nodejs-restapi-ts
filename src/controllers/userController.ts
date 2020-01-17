77ac47cc33d88bbe2761cdc432b58d5b433cf761
artem laschenkov <laschenkov59@yandex.ru>
2020-01-17 14:20:24 +0300

    controller createUser

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -20,3 +20,16 @@ export const getUserById = async (req: Request, res: Response): Promise<Response
     const response: QueryResult = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
     return res.json(response.rows);
 };
+
+//Создать нового пользователя
+export const createUser = async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    const response = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
+    res.json({
+        message: 'User Added successfully',
+        body: {
+            user: { name, email }
+        }
+    })
+};
+
