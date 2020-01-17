ef974a7e9a290a8ef7d7352084ac7cb90bdb17d7
artem laschenkov <laschenkov59@yandex.ru>
2020-01-17 14:15:38 +0300

    controller getUsers

---
+++ src/controllers/userController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { pool } from '../database';
+import { QueryResult } from 'pg';
+
+export const getUsers = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const response: QueryResult = await
+            pool.query('SELECT * FROM users ORDER BY id ASC');
+        return res.status(200).json(response.rows);
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json('Internal Server error');
+    }
+};
+
